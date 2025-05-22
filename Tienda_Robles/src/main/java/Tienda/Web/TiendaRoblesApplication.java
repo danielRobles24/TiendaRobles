0c26c4e7b03b2e759aacddf5363b339e85e57b42
DaRobles24 <danieltico24@gmail.com>
2025-05-21 19:14:45 -0600

    Commit 1
    Mi primer Commit

---
+++ Tienda_Robles/src/main/java/Tienda/Web/TiendaRoblesApplication.java
@@ -0,0 +1,13 @@
+package Tienda.Web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaRoblesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaRoblesApplication.class, args);
+	}
+
+}
